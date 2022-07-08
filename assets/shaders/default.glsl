65cd7523c8d18431d02bbd5fba74845bfb0299bf
Soccan <soccypowa@icloud.com>
2022-07-08 18:08:50 +0200

    8. GLSL shaders

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -17,11 +17,14 @@ void main()
 #type fragment
 #version 330 core
 
+uniform float uTime;
+
 in vec4 fColor;
 
 out vec4 color;
 
 void main()
 {
-    color = fColor;
+    float avg = (fColor.r + fColor.g + fColor.b) / 3;
+    color = vec4(avg, avg, avg, 1);
 }
--- src/main/java/dev/soccan/jade/LevelEditorScene.java
+++ src/main/java/dev/soccan/jade/LevelEditorScene.java
@@ -10,6 +10,7 @@ import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 
 import dev.soccan.renderer.Shader;
+import dev.soccan.util.Time;
 
 public class LevelEditorScene extends Scene {
 
@@ -82,6 +83,7 @@ public class LevelEditorScene extends Scene {
         defaultShader.use();
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
         // Bind our VAO
         glBindVertexArray(vaoID);
         // Enable Vertex attribute pointers
