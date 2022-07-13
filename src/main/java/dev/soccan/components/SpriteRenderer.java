d7082ad2a76fb92653e591c2e097da99272627b2
Soccan <soccypowa@icloud.com>
2022-07-13 15:19:28 +0200

    15. Dirty flags in rendering

--- src/main/java/dev/soccan/components/SpriteRenderer.java
+++ src/main/java/dev/soccan/components/SpriteRenderer.java
@@ -4,11 +4,15 @@ import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 import dev.soccan.jade.Component;
+import dev.soccan.jade.Transform;
 import dev.soccan.renderer.Texture;
 
 public class SpriteRenderer extends Component {
     private Vector4f color;
     private Sprite sprite;
+
+    private Transform lastTransform;
+    private boolean isDirty = false;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
@@ -22,11 +26,15 @@ public class SpriteRenderer extends Component {
 
     @Override
     public void start() {
+        this.lastTransform = gameObject.transform.copy();
     }
 
     @Override
     public void update(float dt) {
-
+        if (!this.lastTransform.equals(this.gameObject.transform)) {
+            this.gameObject.transform.copy(this.lastTransform);
+            isDirty = true;
+        }
     }
 
     public Vector4f getColor() {
@@ -41,4 +49,23 @@ public class SpriteRenderer extends Component {
         return sprite.getTexCoords();
     }
 
+    public void setColor(Vector4f color) {
+        if (!this.color.equals(color)) {
+            this.isDirty = true;
+            this.color.set(color);
+        }
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+        this.isDirty = true;
+    }
+
+    public boolean isDirty() {
+        return this.isDirty;
+    }
+
+    public void setClean() {
+        this.isDirty = false;
+    }
 }
--- src/main/java/dev/soccan/jade/LevelEditorScene.java
+++ src/main/java/dev/soccan/jade/LevelEditorScene.java
@@ -7,6 +7,9 @@ import dev.soccan.util.AssetPool;
 
 public class LevelEditorScene extends Scene {
 
+    private GameObject obj1;
+    private SpriteSheet sprites;
+
     public LevelEditorScene() {
     }
 
@@ -15,9 +18,9 @@ public class LevelEditorScene extends Scene {
         loadResources();
         this.camera = new Camera(new Vector2f(-250, 0));
 
-        SpriteSheet sprites = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
+        sprites = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
 
-        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
@@ -34,8 +37,21 @@ public class LevelEditorScene extends Scene {
                 new SpriteSheet(AssetPool.getTexture("assets/images/spritesheet.png"), 16, 16, 26, 0));
     }
 
+    private int spriteIndex = 0;
+    private float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
+
     @Override
     public void update(float dt) {
+        spriteFlipTimeLeft -= dt;
+        if (spriteFlipTimeLeft <= 0) {
+            spriteFlipTimeLeft = spriteFlipTime;
+            spriteIndex++;
+            if (spriteIndex > 4) {
+                spriteIndex = 0;
+            }
+            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+        }
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
