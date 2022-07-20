635311bb5987b82bb001c8740de235aad0f067af
Soccan <soccypowa@icloud.com>
2022-07-20 11:04:41 +0200

    19. Serialization with GSON

--- src/main/java/dev/soccan/jade/Component.java
+++ src/main/java/dev/soccan/jade/Component.java
@@ -2,7 +2,7 @@ package dev.soccan.jade;
 
 public abstract class Component {
 
-    public GameObject gameObject = null;
+    public transient GameObject gameObject = null;
 
     public void start() {
 
--- src/main/java/dev/soccan/jade/LevelEditorScene.java
+++ src/main/java/dev/soccan/jade/LevelEditorScene.java
@@ -3,6 +3,9 @@ package dev.soccan.jade;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import dev.soccan.components.Sprite;
 import dev.soccan.components.SpriteRenderer;
 import dev.soccan.components.SpriteSheet;
@@ -25,14 +28,25 @@ public class LevelEditorScene extends Scene {
         sprites = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
 
         GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)), 4);
-        obj2.addComponent(new SpriteRenderer(new Sprite(
-                AssetPool.getTexture("assets/images/blendimage2.png"))));
+        SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
+        Sprite obj2Sprite = new Sprite();
+        obj2Sprite.setTexture(AssetPool.getTexture("assets/images/blendimage2.png"));
+        obj2SpriteRenderer.setSprite(obj2Sprite);
+        obj2.addComponent(obj2SpriteRenderer);
         this.addGameObjectToScene(obj2);
 
         obj1 = new GameObject("Object 1", new Transform(new Vector2f(200, 100), new Vector2f(256, 256)), 2);
-        obj1.addComponent(new SpriteRenderer(new Vector4f(1, 0, 0, 1)));
+        SpriteRenderer obj1Sprite = new SpriteRenderer();
+        obj1Sprite.setColor(new Vector4f(1, 0, 0, 1));
+        obj1.addComponent(obj1Sprite);
         this.addGameObjectToScene(obj1);
         this.activeGameObject = obj1;
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        System.out.println(gson.toJson(obj1));
 
     }
 
